37494905e087f0b50a16b7aa9311b4dfd3b04e8e
DoHung <dohung@hungKdev.local>
2022-01-10 20:32:00 +0700

    git

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, Text, SafeAreaView,StyleSheet } from 'react-native'
 const App = () => {
   return (
     <SafeAreaView style={styles.container} >
-      <Text>App11111</Text>
+      <Text>App1111hkfhsdkf1</Text>
     </SafeAreaView>
   )
 }
